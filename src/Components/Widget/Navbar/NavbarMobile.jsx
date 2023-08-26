4d7ba5da9f06ef0582b1cc6b90c74c9f63f4efdb
NapoTwiixe <julien.milants@gmail.com>
2023-08-26 17:59:38 +0200

    add fontAwesome

--- src/Components/Widget/Navbar/NavbarMobile.jsx
+++ src/Components/Widget/Navbar/NavbarMobile.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-
+import { faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 const NavbarMobile = () => {
   return (
     <>
       <nav className='navigations'>
         <ul className='navigation__list'>
-          <li><a href='#'>1</a></li>
-          <li><a href='#'>2</a></li>
+          <li><a href='#'><FontAwesomeIcon icon={faHome} size='2x' className='navigation__logoFontAwesome'/></a></li>
+          <li><a href='#'><FontAwesomeIcon icon={faEnvelope} size='2x' className='navigation__logoFontAwesome'/></a></li>
           <li><a href='#'>3</a></li>
           <li><a href='#'>4</a></li>
         </ul>
