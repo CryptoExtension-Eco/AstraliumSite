89bb66696175512f17ad8de92d182257ddf26133
NapoTwiixe <julien.milants@gmail.com>
2023-08-26 21:41:10 +0200

    add NavBarMobile

--- src/Components/Widget/Navbar/NavbarMobile.jsx
+++ src/Components/Widget/Navbar/NavbarMobile.jsx
@@ -1,18 +1,46 @@
-import React from 'react';
-import { faHome, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
 const NavbarMobile = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const navigationItems = [
+    { icon: faHome, text: 'Home' },
+    { icon: faUser, text: 'About' },
+    { icon: faEnvelope, text: 'Contact' },
+    { icon: faGoogle, text: 'CryptoExtension' },
+  ];
+
   return (
-    <>
-      <nav className='navigations'>
-        <ul className='navigation__list'>
-          <li><a href='#'><FontAwesomeIcon icon={faHome} size='2x' className='navigation__logoFontAwesome'/></a></li>
-          <li><a href='#'><FontAwesomeIcon icon={faEnvelope} size='2x' className='navigation__logoFontAwesome'/></a></li>
-          <li><a href='#'>3</a></li>
-          <li><a href='#'>4</a></li>
-        </ul>
-      </nav>
-    </>
+    <div className="navigation">
+      <ul>
+        {navigationItems.map((item, index) => (
+          <li
+            key={index}
+            className={`list ${index === activeIndex ? 'active' : ''}`}
+            onClick={() => handleItemClick(index)}
+          >
+            <a
+              href={item.text === 'CryptoExtension' ? 'https://cryptoextension.fr/' : '#'}
+              target={item.text === 'CryptoExtension' ? '_blank' : ''}
+              rel={item.text === 'CryptoExtension' ? 'noopener noreferrer' : ''}
+            >
+              <span className="icon">
+                <FontAwesomeIcon icon={item.icon} />
+              </span>
+              <span className="text">{item.text}</span>
+            </a>
+          </li>
+        ))}
+        <div className="indicator"></div>
+      </ul>
+    </div>
   );
 };
 
