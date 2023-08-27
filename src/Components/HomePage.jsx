4bfe103821ede599b3ae619339d2e9c880ee6402
NapoTwiixe <julien.milants@gmail.com>
2023-08-27 11:28:49 +0200

    init home page, change on MobileNavbar

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import '../Scss/Pages/HomePages.scss';
+import TextImg from './img/TextImg.png';
 const HomePage = () => {
   return (
-    <div>
-      <h1 className='paintauchoco'>HomePages</h1>
-    </div>
+    <>
+      <div className="home">
+        <div className="right">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            A condimentum vitae sapien pellentesque habitant morbi tristique senectus.
+            Pellentesque habitant morbi tristique senectus et netus et malesuada fames.
+            Hendrerit dolor magna eget est lorem ipsum dolor. Tincidunt arcu non sodales neque sodales.
+          </p>
+        </div>
+        <div className="left">
+            <img src={TextImg} alt="" />
+          </div>
+      </div>
+    </>
   );
 };
 
