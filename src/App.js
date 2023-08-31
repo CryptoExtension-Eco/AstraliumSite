8e23308a28a667353e25974b79b80888ff199a72
NapoTwiixe <julien.milants@gmail.com>
2023-08-31 15:56:54 +0200

    add preloader (GIGA MEGA TEST)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Scss/App.scss';
+import PreLoader from './Components/Loader/Loader'; // Importez le composant PreLoader ici
 import { Home } from './Components';
 import { About, Contact } from './Components/Pages';
 import { Footer, Navbar } from './Components/Widget';
 
 function App() {
-  const [language, setLanguage] = useState('fr'); // Définissez la langue par défaut
+  const [language, setLanguage] = useState('fr');
+  const [contentLoaded, setContentLoaded] = useState(false); // État pour suivre le chargement du contenu
+
+  useEffect(() => {
+    // Ici, vous pouvez simuler un délai de chargement ou vérifier le chargement de données, etc.
+    // Une fois le chargement terminé, mettez setContentLoaded(true);
+    setTimeout(() => {
+      setContentLoaded(true); // Par exemple, marquez le contenu comme chargé après 2 secondes
+    }, 2000);
+  }, []);
+
   const handleLanguageChange = (newLanguage) => {
     setLanguage(newLanguage);
   };
 
   return (
     <>
-      <Navbar language={language} onLanguageChange={handleLanguageChange} />
-      <Home language={language} onLanguageChange={handleLanguageChange} />
-      <About language={language} />
-      <Contact language={language} />
-      <Footer />
+      {contentLoaded ? ( // Afficher le contenu seulement lorsque le chargement est terminé
+        <>
+          <Navbar language={language} onLanguageChange={handleLanguageChange} />
+          <Home language={language} onLanguageChange={handleLanguageChange} />
+          <About language={language} />
+          <Contact language={language} />
+          <Footer />
+        </>
+      ) : (
+        <PreLoader /> // Afficher le préchargeur pendant le chargement
+      )}
     </>
   );
 }
---
+++ src/Components/Loader/Loader.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <>
+      <div id="preloader">
+        <div id="loader">
+        </div>
+        <h1>Astralium</h1>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
