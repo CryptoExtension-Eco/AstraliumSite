8498dd61f2fa1e53490e4cf15951f8009e651cf7
NapoTwiixe <julien.milants@gmail.com>
2023-09-02 13:59:57 +0200

    delete burger menu from NavbarDesktop

--- src/Components/Widget/Navbar/NavbarDesktop.jsx
+++ src/Components/Widget/Navbar/NavbarDesktop.jsx
@@ -1,53 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-// add le systeme de traduction comme dans NavBarMobile
 const NavbarDesktop = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const handleMenuToggle = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
   return (
-    <>
-      <div>
-        <nav className={`header ${isMenuOpen ? 'header--open' : ''}`}>
-          <a href="#" className="header__logo">
-            Astralium
-          </a>
-          <ul
-            className={`header__navigation ${
-              isMenuOpen ? 'header__navigation--open' : ''
-            }`}
-          >
-            <li className="header__navigation-item">
-              <a href="#" className="header__navigation-link">
-                home
-              </a>
-            </li>
-            <li className="header__navigation-item">
-              <a href="#" className="header__navigation-link">
-                about
-              </a>
-            </li>
-            <li className="header__navigation-item">
-              <a href="#" className="header__navigation-link">
-                contact
-              </a>
-            </li>
-          </ul>
-          <button
-            className={`header__burger ${
-              isMenuOpen ? 'header__burger--open' : ''
-            }`}
-            onClick={handleMenuToggle}
-          >
-            <span className="header__burger-line"></span>
-            <span className="header__burger-line"></span>
-            <span className="header__burger-line"></span>
-          </button>
-        </nav>
-      </div>
-    </>
+    <div>
+      <nav className="header">
+        <a href="#" className="header__logo">
+          Astralium
+        </a>
+        <ul className="header__navigation">
+          <li className="header__navigation-item">
+            <a href="#" className="header__navigation-link">
+              home
+            </a>
+          </li>
+          <li className="header__navigation-item">
+            <a href="#" className="header__navigation-link">
+              about
+            </a>
+          </li>
+          <li className="header__navigation-item">
+            <a href="#" className="header__navigation-link">
+              contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 };
 
