3a52c02ce88c921b850a9a2fed32ef17ddac2797
NapoTwiixe <julien.milants@gmail.com>
2023-09-06 15:49:10 +0200

    paint au chocolat

--- src/Components/Pages/About/AboutDesktop.jsx
+++ src/Components/Pages/About/AboutDesktop.jsx
@@ -1,14 +1,56 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from '../../TranslateService';
+const TechnologyCard = ({ title, items, language }) => {
+  return (
+    <div className="card">
+      <div className="title">
+        <p>{translate(title, language)}</p>
+      </div>
+      <div className="list">
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+TechnologyCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  items: PropTypes.arrayOf(PropTypes.string).isRequired,
+  language: PropTypes.string.isRequired,
+};
 
 const AboutDesktop = ({ language }) => {
+  const technologies = [
+    {
+      title: 'CryptoExtension',
+      items: ['first', 'second', 'third'],
+    },
+    {
+      title: 'Ethereum',
+      items: ['first', 'second', 'third'],
+    },
+    {
+      title: 'Solidity',
+      items: ['first', 'second', 'third'],
+    },
+  ];
+
   return (
-    <>
-      <div className="AboutDesktop">
-        <p>{translate('cryptoExtension', language)}</p>
-      </div>
-    </>
+    <div className="AboutDesktop">
+      {technologies.map((technology, index) => (
+        <TechnologyCard
+          key={index}
+          title={technology.title}
+          items={technology.items.map((item) => translate(item, language))}
+          language={language}
+        />
+      ))}
+    </div>
   );
 };
 
